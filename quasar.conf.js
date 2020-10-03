75caa65642ad9642b187c3e53e6e879a66e660ea
ShaggyTech <beedat1@gmail.com>
2020-10-03 03:33:15 +0000

    feat: scaffold for SSR w/ PWA handoff, default express server

--- quasar.conf.js
+++ quasar.conf.js
@@ -51,7 +51,7 @@ module.exports = function (ctx) {
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
-    supportIE: true,
+    supportIE: false,
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
@@ -73,7 +73,7 @@ module.exports = function (ctx) {
     devServer: {
       https: false,
       port: 8080,
-      open: true // opens browser window automatically
+      open: false // opens browser window automatically
     },
 
     // animations: 'all', // --- includes all animations
@@ -82,7 +82,7 @@ module.exports = function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false
+      pwa: true
     },
 
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
